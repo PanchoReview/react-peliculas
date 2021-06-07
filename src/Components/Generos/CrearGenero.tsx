9d50f4d287f3cad4efce98d05c5b08ca6dd24f4b
Francisco Astorga <fastorga@nimok.com>
2021-06-07 00:46:43 -0400

    Added disable prop to Button component. Wrapped Form in formikProps boundary in CrearGenero.tsx
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

--- src/Components/Generos/CrearGenero.tsx
+++ src/Components/Generos/CrearGenero.tsx
@@ -1,4 +1,4 @@
-import { ErrorMessage, Field, Form, Formik } from "formik"
+import { Form, Formik } from "formik"
 import { useHistory } from "react-router"
 import Button from "../Utils/Button"
 import { Link } from "react-router-dom"
@@ -6,7 +6,7 @@ import * as Yup from "yup"
 import FormGroupText from "../Utils/FormGroupText"
 
 export default function CrearGenero() {
-    const history = useHistory()
+    // const history = useHistory()
 
     return (
         <>
@@ -16,17 +16,23 @@ export default function CrearGenero() {
                 initialValues={{
                     nombre: ""
                 }}
-                onSubmit={values => { console.log(values) }}
+                onSubmit={async values => {
+                    await new Promise(r => setTimeout(r, 3000))
+                    console.log(values)
+                }}
                 validationSchema={Yup.object({
                     nombre: Yup.string().required("Este campo es requerido").primeraLetraMayuscula()
                 })}
             >
-                <Form>
-                    <FormGroupText campo="nombre" label="Nombre" placeholder="Ingrese un nombre" />
+                {(formikProps) => (
+                    <Form>
+                        <FormGroupText campo="nombre" label="Nombre" placeholder="Ingrese un nombre" />
 
-                    <Button type="submit">Salvar</Button>
-                    <Link className="btn btn-secondary" to="/generos">Cancelar</Link>
-                </Form>
+                        <Button disabled={formikProps.isSubmitting} type="submit">Salvar</Button>
+                        <Link className="btn btn-secondary" to="/generos">Cancelar</Link>
+                    </Form>
+                )}
+
             </Formik>
         </>
 
--- src/Components/Utils/Button.tsx
+++ src/Components/Utils/Button.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 export default function Button(props: buttonProps) {
     return (
         <button type={props.type} className="btn btn-primary"
+            disabled={props.disabled}
             onClick={props.onClick}>{props.children}</button>
     )
 }
@@ -11,8 +12,10 @@ interface buttonProps {
     children: ReactNode
     onClick?(): void
     type: "button" | "submit"
+    disabled: boolean
 }
 
 Button.defaultProps = {
-    type: "button"
+    type: "button",
+    disabled: false
 }
