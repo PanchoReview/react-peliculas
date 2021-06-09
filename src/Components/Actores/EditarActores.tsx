2372b69f0eb40869c8696bcdec1a04c696bf09e8
Francisco Astorga <fastorga@nimok.com>
2021-06-08 23:33:09 -0400

    Added FormGroupFecha component

--- src/Components/Actores/EditarActores.tsx
+++ src/Components/Actores/EditarActores.tsx
@@ -4,7 +4,7 @@ import FormularioActores from "./FormularioActores";
 export default function EditarActores() {
     return (
         <>Editar actores
-            <FormularioActores modelo={{ nombre: "Tom Holland", fechaNacimiento: new Date("1994-03-09T:00:00:00") }}
+            <FormularioActores modelo={{ nombre: "Tom Holland", fechaNacimiento: new Date("1994-03-09T00:00:00") }}
                 onSubmit={valores => console.log(valores)}
             />
         </>
--- src/Components/Actores/FormularioActores.tsx
+++ src/Components/Actores/FormularioActores.tsx
@@ -5,18 +5,21 @@ import Button from "../Utils/Button";
 import FormGroupText from "../Utils/FormGroupText";
 import { actorCreacionDTO } from "./actores.model";
 import * as Yup from "yup"
+import FormGroupFecha from "../Utils/FormGroupFecha";
 
 export default function FormularioActores(props: formularioActoresProps) {
-    return(
-        <Formik initialValues={props.modelo} onSubmit={props.onSubmit} 
+    return (
+        <Formik initialValues={props.modelo} onSubmit={props.onSubmit}
             validationSchema={Yup.object({
-                nombre: Yup.string().required("El nombre es requerido").primeraLetraMayuscula()
+                nombre: Yup.string().required("El nombre es requerido").primeraLetraMayuscula(),
+                fechaNacimiento: Yup.date().nullable().required("Este campo es requerido")
+
             })}
         >
             {(formikProps) => (
                 <Form>
                     <FormGroupText campo="nombre" label="Nombre" />
-
+                    <FormGroupFecha label="Fecha nacimiento" campo="fechaNacimiento" />
                     <Button disabled={formikProps.isSubmitting} type="submit" >Salvar</Button>
                     <Link className="btn btn-secondary" to="/actores">Cancelar</Link>
                 </Form>
